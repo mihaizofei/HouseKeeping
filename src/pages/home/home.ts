876e932d9091832579bd19d9201f88226c025837
Mihai Zofei <mihaizofei@yahoo.com>
2018-01-26 07:21:17 +0200

    Change application to WEATHER

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,12 +3,19 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  // styleUrls: ['./home.scss']
 })
 export class HomePage {
+  public title: string;
+  public city: string;
 
   constructor(public navCtrl: NavController) {
+    this.title = 'Craiova';
+  }
 
+  public searchCity(){
+    console.log(`You've searched: ${this.city}`);
   }
 
 }
